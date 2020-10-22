777c06fed414cd4c8990587d8bdfc068bfd22d54
이용현 <dydwk231@naver.com>
2020-10-23 00:00:30 +0900

    20.10.22 about object

--- Javascript/값과 변수 이해하기.js
+++ Javascript/값과 변수 이해하기.js
@@ -5,7 +5,26 @@ console.log(hi)
 console.log(hi2)
 
 
+
 //자바스크립트는 크게 두가지 타입으로 되어 있다. 원시타입 vs 참조타입
 //원시타입? (값이 변수에 할당될때 메모리상에 고정된 크기로 저장) -> 해당 변수가 직접 값을 보관 ex)객체를 제외한 자료형! 숫자형
 //참조타입? 원시 타입과 달리 변수에 고정된 크기를 저장하지 않고, 값의 메모리 주소를 참조한다. -> 따라서 참조 타입은 값의 메모리 주소를 변수에 할당하여 값을 참조한다.
+
+var num  = 5/2
+console.log(num)
+console.log(Math.floor(num))
+
+
+// null & undefined -> undefined은 타입이자 값이다. / null의 타입은 object
+var nullVar = null;
+console.log(typeof nullVar === null) //false
+console.log(nullVar === null) //true
+console.log(typeof nullVar) //object
+
+var nullVar2 = undefined;
+console.log(typeof nullVar2 === undefined) //false
+console.log(nullVar2 === undefined) //true
+console.log(typeof nullVar2) //undefined
+
+
 
--- Javascript/객체 index.js
+++ Javascript/객체 index.js
@@ -1,14 +1,8 @@
 let a = {};
-
 //객체에 키와 값을 추가하기
 a.name = "용현"
-console.log(a)
-
 a["age"]= "27"
 console.log(a)
-
-
-
 
 // 프로토 타입 사용. 키값 뽑아내기
 console.log(Object.keys(a))
@@ -16,7 +10,6 @@ console.log(Object.keys(a))
 
 
 //1)~3) dot notation vs bracket notation
-
 const food = "carrot"
 
 //1)
@@ -27,3 +20,92 @@ a[food] = "delicious"
 a["food"] = "delicious"
 
 console.log(a)
+
+
+//자바스크립트에서는 클래스라는 개념이 없고, 객체 리터럴이나 생성자 함수 등 별도의 생성 방식이 존재한다.
+//객체 생성하는 방법 3가지
+//      1) Object() 생성자 함수 이용
+var foo = new Object()
+foo.name = "foo"
+foo.age = 30
+foo.gender = 'male'
+console.log(foo)
+
+//      2) 객체 리터럴 방식 이용(표기법 사용)
+var foo = {
+    name:'foo',
+    age:30,
+    gender:'male'
+}
+console.log(foo)
+
+//      3) 생성자 함수 이용
+
+
+//객체의 프로퍼티에 접근하기
+var foo = {
+    name:'foo',
+    major:'computer science'
+}
+
+// 객체 프로퍼티 읽기
+console.log(foo.name)
+console.log(foo['name'])
+console.log(foo.nickname)
+
+// 객체 프로퍼티 갱신
+foo.major = 'electronics engineering'
+console.log(foo.major)
+console.log(foo['major'])
+
+// 객체 프로퍼티 동적 생성
+foo.age = 30
+console.log(foo.age)
+
+// 대괄호 표기법만을 사용해야 할 경우 (프로퍼티가 표현식(-)이거나 예약어 일경우)
+console.log(`--------`)
+foo['full-name'] = 'foo bar'
+console.log(foo['full-name'])
+// console.log(foo.full-name) -> 연산오류가 일어날 수 있음. foo.full - name으로 이해할 수있음!
+// console.log(name) //undefined
+
+
+// for in문 과 객체 프로퍼티 출력
+var foo = {
+    name:'foo',
+    age:30,
+    major:'computer science'
+}
+
+for (var prop in foo) {
+    console.log(prop, foo[prop])
+}
+
+//객체 프로퍼티 삭제 -> delete 이용. 객체의 프로퍼티를 삭제한다.객체 자체를 삭제하지는 못한다.
+var foo ={
+    name:'foo',
+    nickname:'babo'
+}
+console.log(`--------`)
+console.log(foo.nickname)
+delete foo.nickname
+console.log(foo.nickname,",", foo)
+
+delete foo
+console.log(foo) //객체 삭제는 실패!
+
+
+//동일한 객체를 참조하는 두 변수
+var objA = {
+    val:40
+}
+var objB = objA //둘다 동일한 값의 주소를 참조하고 있음.
+console.log(objA.val)
+console.log(objB.val)
+
+objB.val = 50 //참조 값을 갱신하였음. -> 동일한 객체를 참조하고 있으므로 둘다 값이 바뀜
+console.log(objA.val)
+console.log(objB.val)
+
+
+
